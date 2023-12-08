0d2a56cda93f767a30c2fa72cf507d92ed755c7f
Jorge Peinado <koke106@gmail.com>
2023-12-08 16:31:23 +0100

    remove redux store

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import testsReducer from './redux/testsSlice'
-
-export const store = configureStore({
-  reducer: {
-    tests: testsReducer
-  },
-})
